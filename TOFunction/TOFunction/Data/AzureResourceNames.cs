cc61835a2f54709aa30d1261aebbc8bc1a15d15f
jordan <jordan_griff2012@hotmail.com>
2020-06-19 02:29:54 +0100

    use new table name

--- TOFunction/TOFunction/Data/AzureResourceNames.cs
+++ TOFunction/TOFunction/Data/AzureResourceNames.cs
@@ -6,6 +6,6 @@
         public readonly static string MorningWaitingTweets = "morning-waiting-tweets";
         public readonly static string MiddayWaitingTweets = "midday-waiting-tweets";
         public readonly static string AfternoonWaitingTweets = "afternoon-waiting-tweets";
-        public readonly static string TweetLogsTable = "TweetLogs";
+        public readonly static string TweetLogsTable = "TweetDetails";
     }
 }
